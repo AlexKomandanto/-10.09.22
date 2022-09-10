8260fc88bc976ee0495459a23bcedf08ad9a8248
Komandanto <komandato666@gmail.com>
2022-09-10 19:08:30 +0300

    Отредактировали файл READMI

--- Program.cs
+++ Program.cs
@@ -1,24 +1,30 @@
-﻿string[] fArray = new string[11] {"hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan"};
-string[] sArray = new string[fArray.Length];
-void FinishArray(string[] fArray, string[] sArray)
+﻿internal class Program
 {
-    int j = 0;
-    for (int i = 0; i < fArray.Length; i++)
+    private static void Main(string[] args)
     {
-    if(fArray[i].Length <= 3)
+        string[] fArray = new string[11] { "hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science", "Russia", "Denmark", "Kazan" };
+        string[] sArray = new string[fArray.Length];
+        void FinishArray(string[] fArray, string[] sArray)
         {
-        sArray[j] = fArray[i];
-        j++;
+            int j = 0;
+            for (int i = 0; i < fArray.Length; i++)
+            {
+                if (fArray[i].Length <= 3)
+                {
+                    sArray[j] = fArray[i];
+                    j++;
+                }
+            }
         }
+        void PrintArray(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]}  ");
+            }
+            Console.WriteLine();
+        }
+        FinishArray(fArray, sArray);
+        PrintArray(sArray);
     }
 }
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]}  ");
-    }
-    Console.WriteLine();
-}
-FinishArray(fArray, sArray);
-PrintArray(sArray);
